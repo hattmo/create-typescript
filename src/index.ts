65e56d12eb87f8c2c2d1f25ee88caf7d95a77d32
Matthew Howard <matthew@hattmo.com>
2019-07-03 15:07:35 -0500

    first draft library templates

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import inquirer from "inquirer";
 import { spawn } from "child_process";
 import { promises as fs } from "fs";
+import inquirer from "inquirer";
 
 export async function start() {
     let dependencies = "";
@@ -10,7 +10,7 @@ export async function start() {
             name: "name",
             type: "input",
             message: "What is the app name?",
-            validate: (answer) => /^([a-z]|\-)+$/.test(answer),
+            validate: (temp) => /^([a-z]|\-)+$/.test(temp),
         }, {
             name: "description",
             type: "input",
@@ -20,104 +20,136 @@ export async function start() {
             choices: ["CLI", "Library", "Express App", "Standalone React App", "React + Express"],
             type: "list",
             message: "What type of app is this?",
-        }]
+        }],
     ) as {
         name: string,
         description: string,
-        type: string
+        type: string,
     };
 
-    let packageJson: any = {};
+    const packageJson: any = {};
     packageJson.version = "1.0.0";
     packageJson.author = "Hattmo";
     packageJson.license = "GPL-3.0-or-later";
     packageJson.name = "@hattmo/" + answer.name;
     packageJson.description = answer.description;
     packageJson.scripts = {};
-    packageJson.scripts["test"] = "cross-per-env";
+    packageJson.scripts.test = "cross-per-env";
     packageJson.scripts["test:staging"] = "mocha -r ts-node/register ./test/**/*Test.ts";
     packageJson.scripts["test:development"] = "mocha -r ts-node/register --bail ./test/**/*Test.ts";
 
     if (answer.type === "CLI") {
 
     } else if (answer.type === "Library") {
-
+        packageJson.scripts.build = "tsc --project .";
+        packageJson.scripts.prepublish = "npm run build";
+        packageJson.main = "./dist/index.js";
+        const libraryAnswer = await inquirer.prompt(
+            [{
+                name: "options",
+                type: "checkbox",
+                message: "Select optional libraries",
+                choices: [
+                    {
+                        short: "React",
+                        value: "  @types/react react",
+                    }, {
+                        short: "React DOM",
+                        value: " @types/react-dom react-dom",
+                    }
+                    , {
+                        short: "React Router",
+                        value: " @types/react-router-dom react-router-dom",
+                    }],
+            }],
+        ) as {
+            name: string,
+            options: string[],
+        };
+        await copyTemplates(__dirname + "/../templates/client", "./", (filename, text) => {
+            if (filename === "README.md_T") {
+                return text.replace(/###name###/, answer.name.toUpperCase())
+                    .replace(/###description###/, answer.description);
+            } else {
+                return text;
+            }
+        });
+        devDependencies += libraryAnswer.options.join("");
     } else if (answer.type === "Express App") {
         packageJson.bin = "dist/server/bin/main.js";
         packageJson.main = "dist/server/app.js";
 
-        packageJson.scripts.start = "cross-per-env"
+        packageJson.scripts.start = "cross-per-env";
         packageJson.scripts["start:production"] = "node --no-warnings dist/server/bin/main.js";
         packageJson.scripts["start:development"] = "nodemon -V dist/server/bin/main.js";
 
-        dependencies += " express"
-        devDependencies += " @types/express"
+        dependencies += " express";
+        devDependencies += " @types/express";
     } else if (answer.type === "Standalone React App") {
         packageJson.scripts.build = "webpack --mode production";
         packageJson.scripts.start = "webpack-dev-server --content-base=dist --inline --watch --hot";
-
         const clientAnswer = await inquirer.prompt(
             [{
                 name: "name",
                 type: "input",
                 message: "What is the window title?",
-                validate: (answer) => /(^[^\t\n\f\/>"'=]+$)/.test(answer),
+                validate: (temp) => /(^[^\t\n\f\/>"'=]+$)/.test(temp),
             }, {
                 name: "options",
                 type: "checkbox",
                 message: "Select optional libraries",
                 choices: [{
                     short: "React Router",
-                    value: " react-router-dom @types/react-router-dom"
-                }]
-            }]
+                    value: " react-router-dom @types/react-router-dom",
+                }],
+            }],
         ) as {
             name: string,
-            options: string[]
+            options: string[],
         };
-
         await copyTemplates(__dirname + "/../templates/client", "./", (filename, text) => {
             if (filename === "webpack.config.js_T") {
                 return text.replace(/###name###/, clientAnswer.name);
             } else if (filename === "README.md_T") {
-                return text.replace(/###name###/, answer.name).replace(/###description###/, answer.description)
+                return text.replace(/###name###/, answer.name.toLocaleUpperCase())
+                    .replace(/###description###/, answer.description);
             } else {
                 return text;
             }
         });
-
-        devDependencies += " @types/react @types/react-dom html-webpack-plugin react react-dom css-loader style-loader file-loader ts-loader webpack webpack-cli webpack-dev-server"
-        devDependencies += clientAnswer.options.join("")
+        // tslint:disable-next-line: max-line-length
+        devDependencies += " @types/react @types/react-dom html-webpack-plugin react react-dom css-loader style-loader file-loader ts-loader webpack webpack-cli webpack-dev-server";
+        devDependencies += clientAnswer.options.join("");
     } else if (answer.type === "React + Express") {
 
     }
 
-    await fs.writeFile('package.json', JSON.stringify(packageJson, null, 2));
-    console.log(`Installing dev dependencies: npm i -D ${devDependencies}`)
+    await fs.writeFile("package.json", JSON.stringify(packageJson, null, 2));
+    process.stdout.write(`Installing dev dependencies: npm i -D ${devDependencies}\n`);
     await promiseExec(`npm i -D ${devDependencies}`);
     if (dependencies) {
-        console.log(`Installing core dependencies: npm i ${dependencies}`)
+        process.stdout.write(`Installing core dependencies: npm i ${dependencies}\n`);
         await promiseExec(`npm i ${dependencies}`);
     }
     const gitAnswer = await inquirer.prompt([{
         name: "gitremote",
         type: "confirm",
-        message: "Set git remote?"
+        message: "Set git remote?",
     }, {
         name: "remoteurl",
         type: "input",
         message: "What is the remote url?",
-        when: (answer: { gitremote: boolean }) => answer.gitremote
+        when: (curr: { gitremote: boolean }) => curr.gitremote,
     }]) as {
         gitremote: boolean,
-        remoteurl: string
-    }
+        remoteurl: string,
+    };
     await promiseExec("git init");
     await promiseExec("git add .");
     await promiseExec("git commit -m \"initial commit\"");
     if (gitAnswer.gitremote) {
         await promiseExec(`git remote add origin ${gitAnswer.remoteurl}`);
-        await promiseExec('git push -u origin master')
+        await promiseExec("git push -u origin master");
     }
 
 }
@@ -126,29 +158,31 @@ export function promiseExec(command: string) {
     return new Promise((resolve, reject) => {
         const child = spawn(command, {
             shell: true,
-            stdio: [process.stdin, process.stdout, process.stderr]
+            stdio: [process.stdin, process.stdout, process.stderr],
         });
         child.on("exit", () => {
             resolve();
         });
         child.on("error", (err) => {
-            reject(err)
-        })
-    })
+            reject(err);
+        });
+    });
 }
 
-export async function copyTemplates(scrPath: string, destpath: string, processTemplate: (filename: string, templateText: string) => string) {
+export async function copyTemplates(scrPath: string,
+                                    destpath: string,
+                                    processTemplate: (filename: string, templateText: string) => string) {
     const files = await fs.readdir(scrPath);
     await Promise.all(files.map((file) => {
-        return (async function () {
-            const stat = await fs.stat(`${scrPath}/${file}`)
+        return (async () => {
+            const stat = await fs.stat(`${scrPath}/${file}`);
             if (stat.isDirectory()) {
                 await fs.mkdir(`${destpath}/${file}`);
                 await copyTemplates(`${scrPath}/${file}`, `${destpath}/${file}`, processTemplate);
             } else {
-                const contents = (await fs.readFile(`${scrPath}/${file}`)).toString('utf8');
-                await fs.writeFile(`${destpath}/${file.replace(/_T/, '')}`, processTemplate(file, contents))
+                const contents = (await fs.readFile(`${scrPath}/${file}`)).toString("utf8");
+                await fs.writeFile(`${destpath}/${file.replace(/_T/, "")}`, processTemplate(file, contents));
             }
-        })()
-    }))
+        })();
+    }));
 }
